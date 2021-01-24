32566902d05ecaf5f15519d5730d6d311d209f56
ehddn3821 <ehddn3821@naver.com>
2021-01-24 21:15:47 +0900

    #8 탭바 제거 및 색상 데이터 추가

--- TwoColors/ColorsData.swift
+++ TwoColors/ColorsData.swift
@@ -10,11 +10,13 @@ import UIKit
 class ColorsData {
     
     let colors1: [String] = [
-        "#02343F", "#331B3F", "#0A174E", "#07553B", "#50586C", "#815854"
+        "#02343F", "#331B3F", "#0A174E", "#07553B", "#50586C", "#815854", "#1E4174", "#A4193D", "#1AAFBC", "#FFDFDE",
+        "#3B1877", "#5F4B8B", "#000000", "#00203F", "#606060", "#2C5F2D", "#00539C", "#0063B2", "#101820", "#CBCE91"
     ]
 
     let colors2: [String] = [
-        "#F0EDCC", "#ACC7B4", "#F5D042", "#CED46A", "#DCE2F0", "#F9EBDE"
+        "#F0EDCC", "#ACC7B4", "#F5D042", "#CED46A", "#DCE2F0", "#F9EBDE", "#DDA94B", "#FFDFB9", "#80634C", "#6A7BA2",
+        "#DA5A2A", "#E69A8D", "#FFFFFF", "#ADEFD1", "#D6ED17", "#97BC62", "#EEA47F", "#9CC3D5", "#FEE715", "#d3687f"
     ]
 }
 
